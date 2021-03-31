3480c384c94a2048c4b88f97c0b2c880a49076a6
jamo95 <jamison.tsai@gmail.com>
2021-03-31 23:48:26 +0800

    declare the contract at the top of conference and migration deployment files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,5 @@
+const Conference = artifacts.require("Conference");
+
 module.exports = function(deployer) {
   deployer.deploy(Conference);
   //deployer.autolink(); // for linking imports of other contracts
